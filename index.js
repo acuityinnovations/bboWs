c611d0bd64910c87abd5b910e71636330121205d
simplem4n <quan.tran@streamy.live>
2020-08-21 16:56:08 +0700

    add orderbook ws

--- index.js
+++ index.js
@@ -1,20 +1,91 @@
 const WebSocket = require('ws');
 
-const LOWER_BOUND = 11650.00;
-const UPPER_BOUND = 12050.00;
 
-const wss = new WebSocket.Server({ port: 5555 });
+const LOWER_BOUND = 11700.00;
+const UPPER_BOUND = 12000.00;
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+const GAP = 20;
+
+const oDWss = new WebSocket.Server({ port: 5556 });
+const bboWss = new WebSocket.Server({ port: 5555 });
+
+oDWss.on('connection', function connection(wss) {
+  wss.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  id = Date.now()
+  oDflooding(wss, 11820.00, 11820.01, true, id)
+});
+
+const randomFromInterval = (min, max) => {
+  return (Math.random() * (max - min + 1) + min).toFixed(2);
+}
+
+const oDflooding = (ws, b, a, direction_up = true, id) => {
+  let step = 0.01;
+  let bid1 = 0;
+  let ask1 = 0;
+
+  if(b.toFixed(2) == (LOWER_BOUND - GAP)) {
+    direction_up = true
+  } else if (b.toFixed(2) == (UPPER_BOUND - GAP)) {
+    direction_up = false
+  } else {
+  }
+
+  if(direction_up) {
+    bid1 = b + step;
+    bid2 = b + 2 * step
+    bid3 = b + 3 * step
+
+    ask1 = a + step;
+    ask2 = a + 2 * step
+    ask3 = a + 3 * step
+  } else {
+    bid1 = b - step;
+    bid2 = b - 2 * step
+    bid3 = b - 3 * step
+
+    ask1 = a - step;
+    ask2 = a - 2 * step
+    ask3 = a - 3 * step
+  }
+
+  let msg = {
+    "data": {
+      "E": 1597984345693,
+      "T": 1597984345687,
+      "U": id - 100,
+      "a": [[ask1, randomFromInterval(0, 5)],
+            [ask2, randomFromInterval(0, 5)],
+            [ask3, randomFromInterval(0, 5)]],
+      "b": [[bid1, randomFromInterval(0, 5)],
+            [bid2, randomFromInterval(0, 5)],
+            [bid3, randomFromInterval(0, 5)]],
+      "e": "depthUpdate",
+      "pu": id - 1,
+      "s": "BTCUSDT",
+      "u": id
+    }
+  }
+  // console.log("delta from exchange: ", msg);
+  console.log("delta bid from exchange: ", bid1);
+  ws.send(JSON.stringify(msg));
+
+  setTimeout(() => {oDflooding(ws, bid1, ask1, direction_up, id + 1)}, 1)
+}
+
+
+bboWss.on('connection', function connection(wss) {
+  wss.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
 
-  flooding(ws, LOWER_BOUND, UPPER_BOUND)
+  bboFlooding(wss, LOWER_BOUND, UPPER_BOUND)
 });
 
 
-const flooding = (ws, b, a, direction_up = true) => {
+const bboFlooding = (wss, b, a, direction_up = true) => {
   let step = 0.01;
   let bid = 0;
   let ask = 0;
@@ -48,7 +119,7 @@ const flooding = (ws, b, a, direction_up = true) => {
   }
   // let msg = "string"
   console.log("best bid from exchange: ", b);
-  ws.send(JSON.stringify(msg));
+  wss.send(JSON.stringify(msg));
 
-  setTimeout(() => {flooding(ws, bid, ask, direction_up)}, 1)
+  setTimeout(() => {bboFlooding(wss, bid, ask, direction_up)}, 1)
 }
